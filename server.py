3c5fd556d36d3b2a7414389fdf38be0c941d2050
Kenia Vargas <kenia.vargascruz27@gmail.com>
2025-01-29 14:33:33 -0600

    feat: Add backend base (Server,Lambdas, Router and Requirement file)

---
+++ server.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.routers.router import router as user_router
+
+
+app = FastAPI()
+
+
+# Include router
+app.include_router(user_router, prefix="/api", tags=["Users"])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the User API!"}
